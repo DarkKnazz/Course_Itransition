9d18678179bb869504c18c7346354ac41c974d8b
DarkKnazz <zhenyaknazz@gmail.com>
2017-07-18 23:59:01 +0300

    First commit in project

---
+++ db/migrate/20170718093839_add_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :about, :string
+    add_column :users, :image, :string
+    add_column :users, :nickname, :string
+  end
+end
