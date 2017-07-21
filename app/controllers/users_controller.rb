6dbbdadbb4dd94f6b9d5e5c2fe83e5ed82252b79
DarkKnazz <zhenyaknazz@gmail.com>
2017-07-21 14:01:16 +0300

    Adding edit and show GUI pages, adding admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:show, :edit, :update]
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
 	def index
 		@users = User.all
@@ -7,14 +8,14 @@ class UsersController < ApplicationController
 
 	def show
 		if(current_user != nil)
-		  if(@user.id == current_user.id)
+		  if (@user.id == current_user.id) || (current_user.isAdmin)
 		  	redirect_to edit_user_path
 		  end
 		end
 	end
 
 	def edit
-		if @user.id != current_user.id
+		if @user.id != current_user.id && current_user.isAdmin == false
 			redirect_to root_path
 		end
 	end
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
 	def update
     	respond_to do |format|
       	  if @user.update(user_params)
-        	format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        	format.html { redirect_to @user }
         	format.json { render :show, status: :ok, location: @user }
       	  else
         	format.html { render :edit }
@@ -30,6 +31,14 @@ class UsersController < ApplicationController
       	  end
     	end
 	end
+
+	def destroy
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_path }
+        format.json { head :no_content }
+      end
+    end
 
 	def set_user
       @user = User.find(params[:id])
--- db/migrate/20170718093839_add_columns_to_users.rb
+++ db/migrate/20170718093839_add_columns_to_users.rb
@@ -5,5 +5,6 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :about, :string
     add_column :users, :image, :string
     add_column :users, :nickname, :string
+    add_column :users, :isAdmin, :boolean, default: false
   end
 end
