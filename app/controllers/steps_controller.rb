cb1eba3540bd9f3abfc3bedbdcde5356054ae129
DarkKnazz <zhenyaknazz@gmail.com>
2017-08-07 10:05:05 +0300

    Fixing bug with security of editing step

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,15 +1,22 @@
 class StepsController < ApplicationController
-  before_action :set_step, only: [:edit, :update, :destroy]
+  before_action :set_step, only: [:edit, :destroy]
+  before_action :authenticate_user!, only: [:edit, :create, :update_step, :destroy]
   skip_before_action :verify_authenticity_token
   respond_to :js, :json, :html
   # GET /steps/1/edit
   def edit
+    if @step.post.user_id != current_user.id && current_user.isAdmin == false
+      redirect_to root_path
+    end
   end
 
   # POST /steps
   # POST /steps.json
   def create
     @post = Post.find(params[:post_id])
+    if @post.user_id != current_user.id && current_user.isAdmin == false
+      redirect_to root_path
+    end
     @step = Step.new(:post=>@post)
     @step = Step.create(step_params)
     @step.content = ""
@@ -27,6 +34,9 @@ class StepsController < ApplicationController
   # DELETE /steps/1
   # DELETE /steps/1.json
   def destroy
+    if @step.post.user_id != current_user.id && current_user.isAdmin == false
+      redirect_to root_path
+    end
     @step.destroy
     respond_to do |format|
       format.html { redirect_to edit_post_path(@step.post.id) }
@@ -36,6 +46,9 @@ class StepsController < ApplicationController
 
   def update_step
     @step = Step.find(params[:id])
+    if @step.post.user_id != current_user.id && current_user.isAdmin == false
+      redirect_to root_path
+    end
     @step.content = params[:data_value]
     @step.save
   end
